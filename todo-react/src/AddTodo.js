61dc034921634d4ce753173638155336c8b716fd
chance <ch4nc3l0@gmail.com>
2018-12-19 16:53:36 -0500

    Feature: addTodo component

--- todo-react/src/AddTodo.js
+++ todo-react/src/AddTodo.js
@@ -5,10 +5,10 @@ export default class AddTodo extends Component{
     render(){
         return(
             <div className='addtodo'>
-                <form className='addtodo--form'>
+                <form className='addtodo--form' onSubmit={this.props.handleAddTodo}>
                     <div className='addtodo--wrapper'>
-                    <input className='addtodo--input' type='text' placeholder='Add Todo' hidden={false}></input>
-                    <input className='addtodo--button' type='submit' value=''></input>
+                    <input className='addtodo--input' id='addTodo' type='text' placeholder='Add Todo' hidden={false}></input>
+                    <input className='addtodo--button' type='submit' value=''></input> 
                     </div>
                 </form>
             </div>
--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -5,12 +5,28 @@ import TodoList from './TodoList';
 import Addtodo from './AddTodo';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.handleAddTodo=this.handleAddTodo.bind(this);
+    this.state={
+      todo:[]
+    };
+  }
+
+    handleAddTodo(e) {
+     e.preventDefault();
+     let todo = document.getElementById('addTodo').value.trim();
+     if(todo!==''){
+     this.setState(prevState=>({todo:[...prevState.todo, todo]}));
+     }
+    }
+  
   render() {
     return (
       <div className="App">
         <Titlebar title='Reactive Todo'/>
         <TodoList/>
-        <Addtodo/>
+        <Addtodo handleAddTodo={this.handleAddTodo}/>
       </div>
     );
   }
